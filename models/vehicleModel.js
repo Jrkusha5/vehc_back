684ea1796ce095b0000a7bfc35166a511799a450
Chernet Tafesse <120357658+Jrkusha5@users.noreply.github.com>
2025-01-20 01:50:18 -0800

    first commit

--- models/vehicleModel.js
+++ models/vehicleModel.js
@@ -4,8 +4,8 @@ const vehicleSchema = new mongoose.Schema({
     name: { type: String, required: true },
     lastUpdated: { type: Date, default: Date.now },
     location: { type: String, default: 'Unknown' },
-    fuelLevel: { type: Number, default: 100 }, // Percentage
-    batteryLevel: { type: Number, default: 100 }, // Percentage
+    fuelLevel: { type: Number}, // Percentage
+    batteryLevel: { type: Number }, // Percentage
 });
 
 const Vehicle = mongoose.model('Vehicle', vehicleSchema);
