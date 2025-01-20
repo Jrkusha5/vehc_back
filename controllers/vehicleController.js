a508da36101dfa015503ff62cd42b1edb0424cf5
Chernet Tafesse <120357658+Jrkusha5@users.noreply.github.com>
2025-01-20 03:02:16 -0800

    first commit

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -40,5 +40,13 @@ const updateVehicle = async (req, res) => {
         res.status(500).json({ message: 'Error updating vehicle', error });
     }
 };
+const getAllVehicles = async (req, res) => {
+    try {
+        const vehicles = await Vehicle.find();
+        res.status(200).json(vehicles);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching vehicles', error });
+    }
+};
 
 module.exports = { addVehicle, updateVehicle, getAllVehicles };
